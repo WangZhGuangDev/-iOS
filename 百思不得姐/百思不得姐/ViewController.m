82f3d79214c549eea879f9f5eb107e53944f3c60
王忠光 <wangzhongguang@wangzhongguangdeMacBook-Pro.local>
2017-01-20 22:43:14 +0800

    初始化项目，添加图标和启动图片

---
+++ 百思不得姐/百思不得姐/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  百思不得姐
+//
+//  Created by 王忠光 on 2017/1/20.
+//  Copyright © 2017年 王忠光. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+@end
